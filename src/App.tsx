e00bc0ea33e4df873f8dcedddc5c34c92f3b2321
atrincas <atrincas@gmail.com>
2024-06-10 21:02:09 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+import { CssBaseline } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
